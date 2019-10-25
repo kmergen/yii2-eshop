bc0df126761fc077b600873ed9f17208ab98ff2d
Klaus Mergen <klaus.mergen@web.de>
2019-10-25 16:31:09 +0200

    Improvement on build process

--- assets/address_test_data.js
+++ assets/address_test_data.js
@@ -1,18 +1,17 @@
-jQuery(function($){
-	// Testdaten in das Anzeigeformular einfügen
-	function addTestdaten() {
-		$('#Address_firstname').val('Klaus');
-		$('#Address_lastname').val('Mergen');
-		$('#Address_street1').val('Andeler Weg 1a');
-		$('#Address_postcode').val('54470');
-		$('#Address_city').val('Bernkastel-Kues');
-	}
-	
-	$('#a-address-test-data').click(function() {
-		addTestdaten();
-	  return false;
-	});
-	//Ende Testdaten einfügen
-});	
- 
-	
+"use strict";
+
+jQuery(function ($) {
+  // Testdaten in das Anzeigeformular einfügen
+  function addTestdaten() {
+    $('#Address_firstname').val('Klaus');
+    $('#Address_lastname').val('Mergen');
+    $('#Address_street1').val('Andeler Weg 1a');
+    $('#Address_postcode').val('54470');
+    $('#Address_city').val('Bernkastel-Kues');
+  }
+
+  $('#a-address-test-data').click(function () {
+    addTestdaten();
+    return false;
+  }); //Ende Testdaten einfügen
+});
--- test1.js
+++
@@ -1,3 +0,0 @@
-const a = 4
-const b = 8
-// this is a comment
