7e9513ff95eda7c9655c4e70bad2be70d0810675
Klaus Mergen <klaus.mergen@web.de>
2019-10-25 13:23:31 +0200

    Remove parcel test

--- parceltest/src/greeting.js
+++
@@ -1,3 +0,0 @@
-export default function (greeting) {
-    return "This is our greeting text: " + greeting
-}
--- parceltest/src/index.js
+++
@@ -1,5 +0,0 @@
-import greet from "./greeting"
-
-const text = greet('Moin')
-console.log(greet('Hallo'))
-document.querySelector('h1').textContent = greet('Moin Moin')
